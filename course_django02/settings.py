11f464dd5a671803d71584305cdf136b19559bd6
course_django02 <course_django02@163.com>
2018-11-15 21:09:06 +0800

    12-数据库配置

--- course_django02/settings.py
+++ course_django02/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '4c*l)+#x(gp^+7kaux7x(0$yv52ob9i-h5gvlh=drfan#o(se+'
-
+# SESSION_ENGINE='django.contrib.sessions.backends.cached_db'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'course_django02.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'course_django02',
+        'USER': 'root',
+        'PASSWORD': 'mysql',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
     }
 }
 
