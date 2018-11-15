9aee9d597058432f4c51fd922be33c74d9681c53
course_django02 <course_django02@163.com>
2018-11-15 08:58:55 +0800

    08查询字符串Query String

--- booktest/urls.py
+++ booktest/urls.py
@@ -5,5 +5,6 @@ from booktest import views
 urlpatterns = [
     url(r'^index/$', views.index),
     url(r'^weather/(?P<city>[a-z]+)/(?P<year>\d{4})/$', views.weather),
-    url(r'^qs', views.qs),
+    url(r'^qs/', views.qs),
+    url(r'^get_body/', views.get_body),
 ]
--- booktest/views.py
+++ booktest/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def index(request):
@@ -8,13 +9,31 @@ def index(request):
 
 
 def weather(request, year, city):
-    print(year,city)
+    print(year, city)
+    a = request.GET.get('a')
+    b = request.GET.get('b')
+    alist = request.GET.getlist('a')
+    print(a, b, alist)
+    c = HttpResponse()
+    c.get('a')
+    print(c)
+    return HttpResponse("%s,,%s" % (year, city))
 
-    return HttpResponse("%s,,%s"%(year,city))
 
 def qs(request):
     a = request.GET.get('a')
     b = request.GET.get('b')
     alist = request.GET.getlist('a')
+    print(a, b, alist)
+    c = HttpResponse()
+    c.get('a')
+    print(c)
+    return HttpResponse('OK')
+
+
+def get_body(request):
+    a = request.POST.get('a')
+    b = request.POST.get('b')
+    alist = request.POST.getlist('a')
     print(a,b,alist)
     return HttpResponse('OK')
